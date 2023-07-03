fb9ce05da941660326d61fbbf09f4cdfb309c812
Guilherme Soares <g.l.soares@outlook.com>
2023-07-03 16:35:18 -0300

    Nova Aula com Eventos

--- meu-primeiro-projeto/src/App.js
+++ meu-primeiro-projeto/src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import Pessoa from './components/Pessoa';
-import Frases from './components/Frases';
-import List from './components/List';
 
 function App() {
 
   return (
     <div className="App">
-      <Pessoa nome='Guilherme' idade='27' profissao='desempregado' foto='https://via.placeholder.com/150' />
-      <Frases />
-      <List/>
+      <h1>Testando Eventos</h1>
 
     </div>
   );
